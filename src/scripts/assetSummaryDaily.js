6e38c3b8ed756be6983a0c05150a7717ad0f5e54
Edwinardyanto <105346100+Edwinardyanto@users.noreply.github.com>
2026-01-20 15:15:11 +0700

    Update assetSummaryDaily.js

--- src/scripts/assetSummaryDaily.js
+++ src/scripts/assetSummaryDaily.js
@@ -1,31 +1,12 @@
-import {
-  getLatestAccountAssetDaily,
-  getLatestAssetPriceDaily,
-} from "./data.js";
+import { getAssetEquitySeries } from "./dataAccess.js";
+import { renderAssetLineChart } from "./charts/assetLineChart.js";
 
-/**
- * Hitung total USD dari snapshot asset harian
- * Asset Summary = current asset value only
- */
-export const calculateTotalAssetUsd = async () => {
-  const dailyAssets = await getLatestAccountAssetDaily();
-  const dailyPrices = await getLatestAssetPriceDaily();
+export const renderAssetSummaryDaily = async (days = 7) => {
+  const series = await getAssetEquitySeries(days);
 
-  let totalUsd = 0;
-
-  const priceMap = new Map(
-    Object.entries(dailyPrices).map(([assetId, p]) => [
-      assetId,
-      Number(p.price_usd || 0),
-    ])
+  renderAssetLineChart(
+    "#asset-summary-line-chart",
+    series.map(p => p.value),
+    series.map(p => p.date)
   );
-
-  for (const account of dailyAssets.accounts || []) {
-    for (const asset of account.assets || []) {
-      const price = priceMap.get(asset.asset_id) || 0;
-      totalUsd += Number(asset.value || 0) * price;
-    }
-  }
-
-  return totalUsd;
 };
