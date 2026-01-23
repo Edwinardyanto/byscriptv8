8123cf99f013745a361a5b540d4f3178480c9dbb
Edwinardyanto <105346100+Edwinardyanto@users.noreply.github.com>
2026-01-24 00:41:50 +0700

    Update assetLineChart.js

--- src/scripts/charts/assetLineChart.js
+++ src/scripts/charts/assetLineChart.js
@@ -124,7 +124,7 @@ export const renderAssetLineChart = ({ container, series, labels }) => {
   hoverDate.style.transform = "translateX(-50%)";
 
   // ✅ anchor exactly where x-axis ticks are
-  hoverDate.style.bottom = "6px";
+  hoverDate.style.bottom = "1px";
 
   /* ---------- OVERLAY CAPTURE ---------- */
 
