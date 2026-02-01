071dc8428d4e5275ee63aaa6f8ca88b076560624
Edwinardyanto <105346100+Edwinardyanto@users.noreply.github.com>
2026-02-01 15:04:41 +0700

    Update accountsSummary.js

--- src/scripts/render/accountsSummary.js
+++ src/scripts/render/accountsSummary.js
@@ -1,5 +1,6 @@
 import { getAccountsWithSummary } from "../dataAccess.js";
 import { renderAccountsDonutChart } from "../charts/accountsDonutChart.js";
+import { colorFromId } from "../color.js";
 
 const formatUsd = (v) =>
   "$" +
@@ -20,11 +21,16 @@ export const renderAccountsSummary = async () => {
   list.innerHTML = "";
 
   for (const acc of accounts) {
+    const accColor = colorFromId(acc.color_id);
+
     const row = document.createElement("div");
     row.className = "summary-item";
 
     row.innerHTML = `
-      <span class="summary-item-name">${acc.account_name}</span>
+      <span class="summary-item-name">
+        <span class="color-dot" style="--dot-color: ${accColor};" aria-hidden="true"></span>
+        ${acc.account_name}
+      </span>
       <span class="summary-item-value">${formatUsd(acc.totalValueUsd)}</span>
     `;
 
